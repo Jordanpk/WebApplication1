484def32fb2bd537c36501c815d3e6d402491a71
jordan carmona <ingjordanilo@bythewave.co>
2023-04-21 07:20:02 -0500

    correccion sonar

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+           // _logger = logger;
         }
 
         public IActionResult Index()
